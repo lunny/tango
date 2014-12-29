83bf0614cc933ef0aec0e90685b0bfad19fef149
Lunny Xiao <xiaolunwen@gmail.com>
2014-12-29 10:48:30 +0800

    group tests

--- group_test.go
+++ group_test.go
@@ -81,3 +81,42 @@ func TestGroup2(t *testing.T) {
 	refute(t, len(buff.String()), 0)
 	expect(t, buff.String(), "/2")
 }
+
+func TestGroup3(t *testing.T) {
+	buff := bytes.NewBufferString("")
+	recorder := httptest.NewRecorder()
+	recorder.Body = buff
+
+	o := Classic()
+	o.Group("/api", func(g *Group) {
+	    g.Group("/v1", func(cg *Group) {
+	        cg.Get("/1", func() string {
+		    return "/1"
+	        })
+	        cg.Post("/2", func() string {
+		    return "/2"
+	        })
+	    })
+	})
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/api/v1/1", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	o.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "/1")
+
+	buff.Reset()
+	req, err = http.NewRequest("POST", "http://localhost:8000/api/v1/2", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	o.ServeHTTP(recorder, req)
+	expect(t, recorder.Code, http.StatusOK)
+	refute(t, len(buff.String()), 0)
+	expect(t, buff.String(), "/2")
+}
